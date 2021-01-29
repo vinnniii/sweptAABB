33b5ebb4ef0f097b8838225623659b8de6a2deca
vinnniii <66980706+vinnniii@users.noreply.github.com>
2021-01-29 19:59:15 +0100

    Delete Collision.js

--- classes/Collision.js
+++
@@ -1,8 +0,0 @@
-export class Collision {
-    constructor(obj_1, obj_2, normal, entryTime) {
-        this.obj_1 = obj_1;
-        this.obj_2 = obj_2;
-        this.normal = normal;
-        this.entryTime = entryTime;
-    }
-}
